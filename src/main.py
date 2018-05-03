3b332b39aecbfb9e1401e771a41e207ba2785d0e
Alex Daigle <alex.daigle2@gmail.com>
2018-05-03 07:01:27 -0400

    Test2 (#1)
    * Fix unittest discovery
    
    * Added pytest fixture example.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
 def sum(a, b):
+	"""
+	This is AMAZING!!!
+	>>> sum(3,5)
+	8
+	>>> sum(7,15)
+	22
+	>>> sum('a', 4)
+	Traceback (most recent call last):
+	...
+	TypeError: must be str, not int
+	"""
 	return a + b
 
 
 def square(a):
+	"""
+	This is AMAZING!!!
+	>>> square(3)
+	9
+	"""
 	return a**2
+
+
+if __name__ == '__main__':
+		import doctest
+		doctest.testmod()
+
--- test/test_main.py
+++ test/test_main.py
@@ -1,19 +1,18 @@
-# import unittest
-# from src.main import sum, square
-#
-#
-# class MyMathTest(unittest.TestCase):
-# 	def test_sum_result(self):
-# 		self.assertEqual(sum(1, 2), 3)
-#
-# 	def test_square_result(self):
-# 		self.assertEqual(square(4), 16)
+
+import unittest
+from src.main import sum, square
 
 
-# from src.main import sum, square
-#
-# def test_sum_result():
-# 	assert sum(1, 2) == 3
-#
-# def test_square_result():
-# 	assert square(4) == 16
+class MyMathTest(unittest.TestCase):
+	def test_sum_result(self):
+		self.assertEqual(sum(1, 2), 3)
+
+	def test_square_result(self):
+		self.assertEqual(square(4), 16)
+
+
+def test_sum_result_pytest():
+	assert sum(1, 2) == 3
+
+def test_square_result_pytest():
+	assert square(4) == 16
