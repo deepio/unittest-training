991e6d18dd9b525950194819187d623a7b11c021
global2alex <global2alex@gmail.com>
2018-05-03 02:40:54 -0400

    Added src/test paradigm.

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def sum(a, b):
+	return a + b
+
+
+def square(a):
+	return a**2
---
+++ test/test_main.py
@@ -0,0 +1,19 @@
+# import unittest
+# from src.main import sum, square
+#
+#
+# class MyMathTest(unittest.TestCase):
+# 	def test_sum_result(self):
+# 		self.assertEqual(sum(1, 2), 3)
+#
+# 	def test_square_result(self):
+# 		self.assertEqual(square(4), 16)
+
+
+# from src.main import sum, square
+#
+# def test_sum_result():
+# 	assert sum(1, 2) == 3
+#
+# def test_square_result():
+# 	assert square(4) == 16
