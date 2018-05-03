aeb081fd4d527370f46841645684d56b9423466a
Alex Daigle <alex.daigle2@gmail.com>
2018-05-03 12:27:06 -0400

    Testing test (#2)
    * I think this is cool...
    * Fix ..

---
+++ test/test_fixtures.py
@@ -0,0 +1,23 @@
+import unittest, pytest, sys
+
+class MyTestCase(unittest.TestCase):
+		@unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
+		def test_windows_support(self):
+		    # windows specific testing code
+		    pass
+
+@pytest.fixture(scope='module')
+def resource_a_setup(request):
+    print('\nresources_a_setup()')
+    def resource_a_teardown():
+        print('\nresources_a_teardown()')
+    request.addfinalizer(resource_a_teardown)
+
+def test_1_that_needs_resource_a(resource_a_setup):
+    print('test_1_that_needs_resource_a()')
+
+def test_2_that_does_not():
+    print('\ntest_2_that_does_not()')
+
+def test_3_that_does(resource_a_setup):
+    print('\ntest_3_that_does()')
